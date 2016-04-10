802626b1eb0448d1f29b596435f8c3c224686294
Hidekazu Kobayashi <kobahide789@gmail.com>
2016-04-10 15:27:32 +0900

    add sanitizers (revenge)

---
+++ sanitizers/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+class BaseSanitizer {
+  constructor(input) {
+    this.input = input;
+  }
+
+  get zeroValue() {
+    throw new Error('Not implemented');
+  }
+
+  get isZero() {
+    return this.zeroValue === this.sanitize();
+  }
+
+  check() {
+    return true;
+  }
+
+  sanitize() {
+    if (this.check()) {
+      return this.input;
+    }
+
+    return this.zeroValue;
+  }
+}
+
+class StringSanitizer extends BaseSanitizer {
+  get zeroValue() {
+    return '';
+  }
+
+  check() {
+    return typeof this.input === 'string';
+  }
+}
+
+class ListSanitizer extends BaseSanitizer {
+  get zeroValue() {
+    return [];
+  }
+
+  get isZero() {
+    return this.sanitize().length === 0;
+  }
+
+  check() {
+    return Array.isArray(this.input);
+  }
+}
